004bfc15ea5879f63d92c3dfd9a9a2f7235e0f07
LittleRookey <37283117+LittleRookey@users.noreply.github.com>
2024-02-22 15:54:55 +0900

    Init Project

---
+++ 2D_Idle/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 플레이어 AI
+    // 달려가서 평타로 시작
+
+    // 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
