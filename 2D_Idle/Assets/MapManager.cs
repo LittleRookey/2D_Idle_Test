94158b72a9d570c517e52d9d3b3fa52510f1f8cf
LittleRookey <37283117+LittleRookey@users.noreply.github.com>
2024-03-16 20:06:30 +0900

    Added map for adventure
    - added map

--- 2D_Idle/Assets/MapManager.cs
+++ 2D_Idle/Assets/MapManager.cs
@@ -8,7 +8,7 @@ public class MapManager : MonoBehaviour
 {
     private Vector2 currentPosition;
 
-    public Transform playerMarker;
+    public RectTransform playerMarker;
 
     public float totalMapSize;
     [SerializeField] private float markerMovePerMeter;
@@ -20,14 +20,92 @@ public class MapManager : MonoBehaviour
     public float leftDist; // 목적지까지 남은 거리
     public Vector2 firstDestination;
     public TextMeshProUGUI currentPosText;
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI zoomInText;
+    public int zoomIn = 5;
+    [Header("Camera Movement")]
+    [SerializeField] private Camera cam;
+    [SerializeField] private RectTransform actualMap;
+    [SerializeField] private RectTransform screenMap;
+    [SerializeField] private int maxZoomMod = 5;
+    [SerializeField] private int minZoomMod = 1;
+    private Vector3 dragOrigin;
+    private float mapMinX, mapMaxX, mapMinY, mapMaxY;
+    private float mapWidth, mapHeight;
+    private bool canScroll = true;
+    private void Awake()
     {
+        mapWidth = screenMap.rect.width/2;
+        mapHeight = screenMap.rect.height/2;
+        
         currentPosition = startPos;
+        actualMap.anchoredPosition = currentPosition;
+        playerMarker.anchoredPosition = currentPosition;
+
         currentPosText.SetText($"{currentPosition.x.ToString("F0")}, {currentPosition.y.ToString("F0")}");
         SetDestination(firstDestination);
 
+        zoomIn = 1;
+        zoomInText.SetText($"x{zoomIn}");
+        DOTween.To(() => actualMap.transform.localScale, x => actualMap.transform.localScale = x, Vector3.one * zoomIn, 0.2f);
+        //mapMinX = actualMap.transform.position.x - actualMap.bounds.size.x;
+        //mapMaxX = actualMap.transform.position.x + actualMap.bounds.size.x;
+
+        
     }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ZoomIn()
+    {
+        if (zoomIn + 1 > maxZoomMod) zoomIn = 5;
+        else zoomIn += 1;
+
+        if (zoomIn > 1)
+        {
+            canScroll = true;
+            //actualMap.anchoredPosition *= zoomIn;
+            //if (actualMap.anchoredPosition.x >0)
+            //{
+            //    actualMap.anchoredPosition += (Vector2.right * (mapWidth * (zoomIn - 1)));
+            //} else if (actualMap.anchoredPosition.x < 0)
+            //{
+            //    actualMap.anchoredPosition += (Vector2.left * (mapWidth * (zoomIn - 1)));
+            //}
+            //if (actualMap.anchoredPosition.y > 0)
+            //{
+            //    actualMap.anchoredPosition += (Vector2.up * (mapHeight * (zoomIn - 1)));
+            //} else if (actualMap.anchoredPosition.y < 0)
+            //{
+            //    actualMap.anchoredPosition += (Vector2.down * (mapHeight * (zoomIn - 1)));
+            //}
+        }
+        zoomInText.SetText($"x{zoomIn}");
+        // change position based on scale
+        //actualMap.transform.localScale = Vector3.one * zoomIn;
+        DOTween.To(() => actualMap.transform.localScale, x => actualMap.transform.localScale = x, Vector3.one * zoomIn, 0.2f).OnComplete(()=>ClampMap());
+
+    }
+
+    public void ZoomOut()
+    {
+        if (zoomIn - 1 < minZoomMod) zoomIn = 1;
+        else zoomIn -= 1;
+
+        if (zoomIn == 1)
+        {
+            canScroll = false;
+            actualMap.anchoredPosition = Vector3.zero;
+        }
+        zoomInText.SetText($"x{zoomIn}");
+        //zoomIn--;
+        //actualMap.transform.localScale = Vector3.one * zoomIn;
+        DOTween.To(() => actualMap.transform.localScale, x => actualMap.transform.localScale = x, Vector3.one * zoomIn, 0.2f).OnComplete(() => ClampMap());
+    }
+
+
 
     public void SetDestination(Vector2 destPos)
     {
@@ -37,10 +115,18 @@ public class MapManager : MonoBehaviour
         StartCoroutine(MoveTo());
     }
 
+    public void SetCenter()
+    {
+        // 마커 * 스케ㅐ일 만큼 뺴기
+        actualMap.anchoredPosition = Vector2.zero;
+        actualMap.anchoredPosition -= (playerMarker.anchoredPosition*zoomIn);
+        ClampMap();
+    }
+
     private IEnumerator MoveTo()
     {
         yield return new WaitForSeconds(1f);
-        var movedDist = moveDir * walkPerSec;
+        var movedDist = moveDir * walkPerSec * zoomIn;
         //DOTween.To(() => damaged.fillAmount, x => damaged.fillAmount = x, current / max, 0.2f);
         playerMarker.DOLocalMove(moveDir * markerMovePerMeter * walkPerSec, 1f).SetEase(Ease.Linear).SetRelative(true);
         currentPosition += movedDist;
@@ -56,9 +142,32 @@ public class MapManager : MonoBehaviour
         }
         StartCoroutine(MoveTo());
     }
+
+    private void ClampMap()
+    {
+        actualMap.anchoredPosition = new Vector2(Mathf.Clamp(actualMap.anchoredPosition.x, -1 * mapWidth * (zoomIn - 1), mapWidth * (zoomIn - 1)),
+                Mathf.Clamp(actualMap.anchoredPosition.y, -1 * mapHeight * (zoomIn - 1), mapHeight * (zoomIn - 1)));
+    }
+    private void PanCamera()
+    {
+        // Save position of mouse in world space when drag starts (first time click)
+        if (Input.GetMouseButtonDown(0))
+            dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        // calculate distance between drag origin and new position if it is still held down
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 diff = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
+
+            print("origin " + dragOrigin + " new Position " + cam.ScreenToWorldPoint(Input.mousePosition) + "=difference " + diff);
+            actualMap.anchoredPosition += diff;
+            ClampMap();
+            //cam.transform.position += diff;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        PanCamera();
     }
 }
